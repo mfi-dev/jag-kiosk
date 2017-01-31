550c8734b3429cc5ed86d3801abfc85e3c1294b2
Wesley Hall <wesleyahall@gmail.com>
2017-01-31 15:53:40 -0600

    initial home page

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -5,12 +5,26 @@ class HomePage extends React.Component {
   render () {
     return (
       <div className='HomePage'>
+        <div className='GoldLine GoldLine--Top'></div>
         <h1>JAG Kiosk</h1>
-        <nav className='Menu'>
-          <a href='#'>Test</a>
-          <a href='#'>Test2</a>
-          <a href='#'>Test3</a>
+        <div className='Seal'>
+          <img src='../../assets/images/jag-seal.png' />
+        </div>
+        <nav className='MainMenu'>
+          <div className='MainMenu__LinkContainer'>
+            <a className='MainMenu__Link' href='#'>The Judge Advocate Generals</a>
+          </div>
+          <div className='MainMenu__LinkContainer'>
+            <a className='MainMenu__Link' href='#'>Senior Paralegal Managers</a>
+          </div>
+          <div className='MainMenu__LinkContainer'>
+            <a className='MainMenu__Link' href='#'>Award Winners</a>
+          </div>
+          <div className='MainMenu__LinkContainer'>
+            <a className='MainMenu__Link' href='#'>Jag Corps Pulse</a>
+          </div>
         </nav>
+        <div className='GoldLine GoldLine--Bottom'></div>
       </div>
     )
   }
