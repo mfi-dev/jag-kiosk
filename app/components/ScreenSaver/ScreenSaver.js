6037addd5c9d060cab7a469285de63cf15be4c04
Wesley Hall <wesleyahall@gmail.com>
2017-03-09 15:31:03 -0600

    Adding ken burns screensaver

--- app/components/ScreenSaver/ScreenSaver.js
+++ app/components/ScreenSaver/ScreenSaver.js
@@ -1,4 +1,4 @@
-/* global $, Swiper */
+/* global $ */
 import React from 'react'
 import './ScreenSaver.styl'
 
@@ -6,21 +6,14 @@ class ScreenSaver extends React.Component {
 
   componentDidMount () {
     $('.ScreenSaverPage').bind('touchstart click', this.handleClick)
-    var mySwiper = new Swiper('.swiper-container', {
-      direction: 'horizontal',
-      loop: false,
-      scrollbarHide: true,
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      resistance: false,
-      height: 1080,
-      width: 1920,
-      autoplay: 9000,
-      setWrapperSize: true,
-      effect: 'coverflow'
+    $('#ScreenSaver').Kenburns({
+      images: this.props.images,
+      scale: 0.90,
+      duration: 8000,
+      fadeSpeed: 1200,
+      ease3d: 'ease-out'
     })
-    mySwiper.pagination = false
-    mySwiper.scrollbarHide = true
+    console.log($('#kenburns-slideshow').html)
   }
 
   componentWillUnmount () {
@@ -34,34 +27,8 @@ class ScreenSaver extends React.Component {
   }
 
   render () {
-    const swiperSlides = (this.props.images.length)
-      ? this.props.images.map((image, index) => {
-        const style = {
-          backgroundImage: `url(${image})`,
-          height: 1080,
-          width: 1920
-        }
-        const id = `slide${index}`
-        return (
-          <div
-            key={image + index}
-            id={id}
-            style={style}
-            className='swiper-slide'
-          />
-        )
-      })
-      : <div>Loading images...</div>
-
     return (
-      <div id='ScreenSaver' className='ScreenSaver'>
-        <div className='swiper-container'>
-          <div className='swiper-wrapper'>
-            {swiperSlides}
-          </div>
-          <div className='swiper-scrollbar' />
-        </div>
-      </div>
+      <div id='ScreenSaver' />
     )
   }
 }
